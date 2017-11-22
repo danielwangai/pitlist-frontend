997ecdab743eb63430de1c192c73855ed6c1b2ab
Daniel Wangai <mainad57@gmail.com>
2017-11-22 13:46:39 +0300

    chore(navbar): refactor navbar - only display login and register links if not authenticated.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,14 +3,32 @@ import React, { Component } from 'react';
 const NavLink = require('react-router-dom').NavLink;
 
 export default class Nav extends Component {
+	constructor() {
+		super();
+		this.checkIsLoggedIn = this.checkIsLoggedIn.bind(this);
+	}
+	checkIsLoggedIn() {
+		const userDetails = localStorage.getItem('currentUser');
+		// console.log(userDetails, "User Details")
+		return userDetails;
+	}
 	render() {
+		if(!this.checkIsLoggedIn()) {
+			return (
+				<ul>
+					<li>
+						<NavLink exact activeClassName="active" to="/register">register</NavLink>
+					</li>
+					<li>
+						<NavLink exact activeClassName="active" to="/login">login</NavLink>
+					</li>
+				</ul>
+			)
+		}
 		return (
 			<ul>
 				<li>
-					<NavLink exact activeClassName="active" to="/register">register</NavLink>
-				</li>
-				<li>
-					<NavLink exact activeClassName="active" to="/login">login</NavLink>
+					<NavLink exact activeClassName="active" to="/bucketlists">Bucketlists</NavLink>
 				</li>
 			</ul>
 		)
